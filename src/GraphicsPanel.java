2916d940d410ac382b5fadf93ca01e59f2edd4af
unknown <BT_3W2_15@BKTECH.LOCAL>
2024-05-20 09:11:19 -0400

    Initial commit

---
+++ src/GraphicsPanel.java
@@ -0,0 +1,174 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GraphicsPanel extends JPanel implements KeyListener, MouseListener, ActionListener {
+    private BufferedImage background;
+    private BufferedImage background2;
+    private Player player;
+    private boolean[] pressedKeys;
+    private ArrayList<Coin> coins;
+    private Timer timer;
+    private int time;
+    private JButton resetButton;
+    private JButton pauseButton;
+    private static boolean pauseGame;
+
+    public GraphicsPanel(String name) {
+        try {
+            background = ImageIO.read(new File("src/background.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }try {
+            background2 = ImageIO.read(new File("src/underWater.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        player = new Player("src/marioleft.png", "src/marioright.png", name);
+        coins = new ArrayList<>();
+        pressedKeys = new boolean[128];
+        time = 0;
+        timer = new Timer(1000, this); // this Timer will call the actionPerformed interface method every 1000ms = 1 second
+        timer.start();
+        resetButton = new JButton("Reset");
+        resetButton.addActionListener(this);
+        add(resetButton);
+        resetButton.setFocusable(false);
+        pauseButton = new JButton("Pause");
+        pauseButton.addActionListener(this);
+        add(pauseButton);
+        pauseButton.setFocusable(false);
+        pauseGame = false;
+        addKeyListener(this);
+        addMouseListener(this);
+        setFocusable(true); // this line of code + one below makes this panel active for keylistener events
+        requestFocusInWindow(); // see comment above
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);  // just do this
+        if(player.getScore()<10) {
+            g.drawImage(background, 0, 0, null);  // the order that things get "painted" matter; we put background down first
+        }else{
+            g.drawImage(background2, 0, 0, null);
+        }
+        g.drawImage(player.getPlayerImage(), player.getxCoord(), player.getyCoord(), null);
+        resetButton.setLocation(23,78);
+        pauseButton.setLocation(23,108);
+        if(pauseGame){
+            g.setFont(new Font("Courier New", Font.BOLD, 60));
+            g.drawString("PAUSED",380,270);
+        }
+        // this loop does two things:  it draws each Coin that gets placed with mouse clicks,
+        // and it also checks if the player has "intersected" (collided with) the Coin, and if so,
+        // the score goes up and the Coin is removed from the arraylist
+        if(!pauseGame) {
+            for (int i = 0; i < coins.size(); i++) {
+                Coin coin = coins.get(i);
+                g.drawImage(coin.getImage(), coin.getxCoord(), coin.getyCoord(), null); // draw Coin
+                if (player.playerRect().intersects(coin.coinRect())) { // check for collision
+                    player.collectCoin();
+                    coins.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        // draw score
+        g.setFont(new Font("Courier New", Font.BOLD, 24));
+        g.drawString(player.getName() + "'s Score: " + player.getScore(), 20, 40);
+        g.drawString("Time: " + time, 20, 70);
+
+        // player moves left (A)
+        if (pressedKeys[65]) {
+            player.faceLeft();
+            player.moveLeft();
+        }
+
+        // player moves right (D)
+        if (pressedKeys[68]) {
+            player.faceRight();
+            player.moveRight();
+        }
+
+        // player moves up (W)
+        if (pressedKeys[87]) {
+            player.moveUp();
+        }
+
+        // player moves down (S)
+        if (pressedKeys[83]) {
+            player.moveDown();
+        }
+    }
+    public static boolean getPauseGame(){
+        return pauseGame;
+    }
+
+    // ----- KeyListener interface methods -----
+    public void keyTyped(KeyEvent e) { } // unimplemented
+
+    public void keyPressed(KeyEvent e) {
+        // see this for all keycodes: https://stackoverflow.com/questions/15313469/java-keyboard-keycodes-list
+        // A = 65, D = 68, S = 83, W = 87, left = 37, up = 38, right = 39, down = 40, space = 32, enter = 10
+        int key = e.getKeyCode();
+        pressedKeys[key] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        pressedKeys[key] = false;
+    }
+
+    // ----- MouseListener interface methods -----
+    public void mouseClicked(MouseEvent e) { }  // unimplemented; if you move your mouse while clicking,
+    // this method isn't called, so mouseReleased is best
+
+    public void mousePressed(MouseEvent e) { } // unimplemented
+
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {  // left mouse click
+            Point mouseClickLocation = e.getPoint();
+            Coin coin = new Coin(mouseClickLocation.x, mouseClickLocation.y);
+            coins.add(coin);
+        } else {
+            Point mouseClickLocation = e.getPoint();
+            if (player.playerRect().contains(mouseClickLocation)) {
+                player.turn();
+            }
+        }
+    }
+
+    public void mouseEntered(MouseEvent e) { } // unimplemented
+
+    public void mouseExited(MouseEvent e) { } // unimplemented
+
+    // ACTIONLISTENER INTERFACE METHODS: used for buttons AND timers!
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof Timer) {
+            time++;
+        }
+        if(e.getSource() instanceof JButton){
+            JButton button = (JButton)e.getSource();
+            if(button == resetButton){
+                coins.clear();
+                player.setScore(0);
+                time = 0;
+            }else if(button == pauseButton){
+                if(!pauseGame){
+                    timer.stop();
+                    pauseGame = true;
+                }else{
+                    timer.start();
+                    pauseGame = false;
+                }
+            }
+        }
+    }
+}
